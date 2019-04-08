5d8b749cf90fb16f3afb4f9f58f7edf2694f4f92
Ethan Ferguson <ethanf108@gmail.com>
2019-04-08 11:02:24 -0700

    Reworked image display

--- src/launch/DisplayImg.java
+++ src/launch/DisplayImg.java
@@ -1,35 +1,26 @@
 package launch;
 
 import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
-public class DisplayImg extends JPanel{
+public final class DisplayImg extends JFrame{
     private final BufferedImage dispImg;
-    private final JFrame frame;
-    
-    private void setupFrame(){
-        frame.getContentPane().setLayout(new FlowLayout());
-        frame.getContentPane().add(this);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-    }
+
     public DisplayImg(BufferedImage img){
-        super();
-        this.frame = new JFrame("Image");
+        super("Image");
         this.dispImg = img;
-        this.setPreferredSize(new Dimension(this.dispImg.getWidth(), this.dispImg.getHeight()));
-        setupFrame();
-        this.frame.repaint();
+        this.setSize(new Dimension(this.dispImg.getWidth(), this.dispImg.getHeight()));
+        setResizable(false);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        this.repaint();
     }
     
     @Override
-    public void paintComponent(Graphics go){
+    public void paint(Graphics go){
         Graphics2D g = (Graphics2D)go;
         g.drawImage(dispImg, null, 0, 0);
     }
